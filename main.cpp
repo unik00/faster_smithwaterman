e8b19d18da91e9e8847b21a94efe021d93bde4ae
Ngo Duc Huy <ngoduchuy64@gmail.com>
2020-12-09 18:44:44 +0700

    fixed

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
+#include <cassert>
+#include "vn_lang_tool.hpp"
 
 using namespace std;
 
 const int N = 1005;
 int dp[N][N];
 
-int old_leven(string a, string b){
+int old_leven(string sa, string sb){
+    string a = " " + sa;
+    string b = " " + sb;
+
     // traditional smith waterman for benchmark
-    a = " " + a;
-    b = " " + b;
     int ans = 0;
      for(int i = 1; i < int(a.size()); i++){
         for(int j = 1; j < int(b.size()); j++){
@@ -26,18 +30,20 @@ int old_leven(string a, string b){
 }
 
 int w[N];
-bool in_b[N];
+bool in_b[int(1e6)];
 char a[N];
 
-int attempt(string _a, string b){
-    b = " " + b;
+int attempt(string sa, string sb){
+    string b = " " + sb;
+    string _a = sa;
+
     for(int i = 1; i < (int)b.size(); i++) {
         in_b[b[i]] = true;
     }
     int ans = 0;
     int cur_w = 0, m = 0;
 
-    for(char i : _a){
+    for(auto i : _a){
         if (in_b[i]) {
             w[++m] = cur_w;
             a[m] = i;
@@ -47,8 +53,6 @@ int attempt(string _a, string b){
             cur_w++;
         }
     }
-//    for(int i = 0; i < (int)_a.size(); i++) cout << w[i] << " ";
-//    cout << endl;
 
     for(int i = 1; i <= m; i++){
         for(int j = 1; j < int(b.size()); j++){
@@ -57,13 +61,8 @@ int attempt(string _a, string b){
             dp[i][j] = max(dp[i][j], dp[i-1][j-1] - 1 - w[i]);
 
             if (a[i] == b[j]) {
-                if (dp[i - 1][j - 1] == 0){
-                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1);
-                }
-                else{
-                    for(int l = 0, min_ = min(w[i], j - 2); l <= min_; l++) {
-                        dp[i][j] = max(dp[i][j], dp[i - 1][j - 1 - l] + 1 - w[i]);
-                    }
+                for(int l = 0, min_ = min(w[i], j - 1); l <= min_; l++) {
+                    dp[i][j] = max(dp[i][j], max(0, dp[i - 1][j - 1 - l] - w[i]) + 1);
                 }
             }
             dp[i][j] = max(dp[i][j], 0);
@@ -82,54 +81,53 @@ int attempt(string _a, string b){
 }
 
 void test_single(string a, string b){
+
+
     clock_t start;
     double duration;
 
+    start = clock();
+    int new_ans =attempt(a, b);
+    cout << new_ans << endl;
+    double new_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
+    cout<< "Attempt time: "<< new_duration <<'\n';
 
     start = clock();
-    cout << old_leven(a, b) << endl;
+    int old_ans = old_leven(a, b);
+    cout << old_ans << endl;
     double old_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
     cout<< "Old time: " << old_duration <<'\n';
 
-    start = clock();
-    cout << attempt(a, b) << endl;
-    double new_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
-    cout<< "Attempt time: "<< new_duration <<'\n';
+
     cout << "Percentage: " << new_duration / old_duration * 100 <<  "%" << endl;
     cout << "**********************\n";
+    assert(new_ans == old_ans);
 }
 
 int main() {
+    VnLangTool::init("", true);
+
     string a = "a", b = "b";
 
     a = "biznm";
     b = "bienm";
     test_single(a, b);
 
-    a = "bizznxh";
-    b = "bienxh";
-    test_single(a, b);
-
-    a = "nha so 8 ngo 269 pho thanh nhan phuong thanh nhan quan hai ba trung ha noi";
-    b = "hai ba trung";
+    a = "giaoc";
+    b = "ngoc";
     test_single(a, b);
 
 
-    a = "nha so 8 ngo 269 pho thanh nhan phuong thanh nhan quan hai ba trung ha noi";
-    b = "pho thah nkan";
-    test_single(a, b);
-
-    a = "so 100 thach loi thanh xuan soc son ha noi";
-    b = "thanh loi";
-    test_single(a, b);
-
-    a = "số nhà 16 ngõ 435 đường xuân đỉnh đường xuân đỉnh quận bắc từ liêm thành phố hà nội xuân đỉnh phường xuân đỉnh quận từ liêm hà nội,Hà Nội";
-    b = "xuan dinh";
-    test_single(a, b);
-
-    a = "lê ngọc huyền số 9 đường nguyễn thị sóc ấp bắc lân xã bà điểm huyện hóc môn tphcm xã bà điểm huyện hóc môn tp hồ chí minh,TP Hồ Chí Minh";
-    b = "huyen hoc mon";
-    test_single(a, b);
-
+    ifstream fin("test_1.txt");
+    string s;
+    while (getline(fin, s)){
+        cout << s << endl;
+        int sub_len = rand() % 15 + 5;
+        for(int i = 0; i < (int)s.size() - sub_len + 1; i++){
+            string b = "doi can";
+            cout << s << "\n" << b << endl;
+            test_single(s, b);
+        }
+    }
     return 0;
 }

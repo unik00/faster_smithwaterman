41872c644ec795e2214cd3e538fa0a6d4fc517e2
Ngo Duc Huy <ngoduchuy64@gmail.com>
2020-12-10 13:59:15 +0700

    minor fix

--- main.cpp
+++ main.cpp
@@ -22,6 +22,10 @@ int min(const int &a, const int &b){
 }
 
 int old_leven(string sa, string sb){
+    clock_t start;
+    double duration;
+    start = clock();
+
     string a = " " + sa;
     string b = " " + sb;
 
@@ -39,7 +43,8 @@ int old_leven(string sa, string sb){
     }
     for(int i = 0; i <= int(a.size()); i++) dp[i][0] = 0;
     for(int j = 0; j < int(b.size()); j++) dp[0][j] = 0;
-
+    double s_old_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
+    old_duration += s_old_duration;
     return ans;
 }
 
@@ -48,6 +53,10 @@ bool in_b[int(1e6)];
 char a[N];
 
 int attempt(string sa, string sb){
+    clock_t start;
+    double duration;
+    start = clock();
+
     string b = " " + sb;
     string _a = sa;
 
@@ -58,7 +67,7 @@ int attempt(string sa, string sb){
     int cur_w = 0, m = 0;
 
     for(auto c : _a){
-        if (c == ' ' && a[m] == ' ') continue;
+//        if (c == ' ' && a[m] == ' ') continue;
         if (in_b[c]) {
             w[++m] = cur_w;
             a[m] = c;
@@ -68,7 +77,8 @@ int attempt(string sa, string sb){
             cur_w++;
         }
     }
-//    for (int i = 1; i <m; i++) cout <<a[i]; cout<<endl;
+//    for (int i = 1; i <=m; i++) cout <<a[i]; cout<<endl;
+//    for (int i = 1; i <=m; i++) cout <<w[i] << " "; cout<<endl;
 //    cout << b << endl;
 
     for(int i = 1; i <= m; i++){
@@ -77,7 +87,7 @@ int attempt(string sa, string sb){
                 dp[i][j] = dp[i][j - 1] - 1;
             }
             else {
-                dp[i][j] = dp[i - 1][j] - 1;
+                dp[i][j] = dp[i - 1][j] - 1 - w[i];
             }
 
             if (dp[i][j] < dp[i-1][j-1] - 1 - w[i]){
@@ -85,11 +95,11 @@ int attempt(string sa, string sb){
             }
 
             if (a[i] == b[j]) {
+                // we need to query max(dp[i-1][j-1-w[i] to j-1]), can we do this faster?
                 for(int l = 0, min_ = min(w[i], j - 1); l <= min_; l++) {
                     if (dp[i][j] < max(0, dp[i - 1][j - 1 - l] - w[i]) + 1){
                         dp[i][j] = max(0, dp[i - 1][j - 1 - l] - w[i]) + 1;
                     }
-
                 }
             }
             if (dp[i][j] < 0) dp[i][j] = 0;
@@ -105,39 +115,13 @@ int attempt(string sa, string sb){
         in_b[b[i]] = false;
         w[i] = 0;
     }
+
+    double s_new_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
+    new_duration += s_new_duration;
+
     return ans;
 }
 
-void test_single(string a, string b){
-    a = VnLangTool::lower_root(a);
-    b = VnLangTool::lower_root(b);
-
-
-    clock_t start;
-    double duration;
-
-    start = clock();
-    int old_ans = old_leven(a, b);
-    cout << old_ans << endl;
-    double s_old_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
-    old_duration += s_old_duration;
-    cout<< "Old time: " << s_old_duration <<'\n';
-
-
-    start = clock();
-    int new_ans =attempt(a, b);
-    cout << new_ans << endl;
-    double s_new_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
-    new_duration += s_new_duration;
-    cout<< "Attempt time: "<< s_new_duration <<'\n';
-
-
-
-
-//    cout << "Percentage: " << new_duration / old_duration * 100 <<  "%" << endl;
-    cout << "**********************\n";
-    assert(new_ans == old_ans);
-}
 
 string gen_random(const int len) {
 
@@ -159,70 +143,36 @@ string gen_random(const int len) {
 
 int main() {
     VnLangTool::init("", true);
+    string a, b;
 
-    string a = "a", b = "b";
+    ifstream fin("test_1.txt");
+    vector<int> res1, res2;
 
-    a = "biznm";
-    b = "bienm";
-    test_single(a, b);
-
-    a = "giaoc";
-    b = "ngoc";
-    test_single(a, b);
-    a = "số 16 nguỵ như kon tum thanh xuân hn nhân chính hà đông hà nội hà nội";
-    b = "nhan chinh";
-    test_single(a, b);
-
-    a = "chua lang dong da ha noi ha noi";
-    b = "^láng hạ|^lh";
-    test_single(a, b);
-
-    a = "chua lang dong da ha noi ha noi";
-    b = "lh";
-    test_single(a, b);
+    while (getline(fin, a)){
+        getline(fin, b);
+        a = VnLangTool::lower_root(a);
+        b = VnLangTool::lower_root(b);
+        res2.push_back(attempt(a, b));
+//        res1.push_back(old_leven(a, b));
 
 
-    a = "193 tl 19 tạp hoá văn cường gặp chị anh thạnh lộc gò vấp hồ chí minh hồ chí minh";
-    b = "^phường 16|^p1";
-    test_single(a, b);
+    }
+    fin.close();
+/*
+    fin.open("test_1.txt");
+    while (getline(fin, a)){
+        getline(fin, b);
+        a = VnLangTool::lower_root(a);
+        b = VnLangTool::lower_root(b);
+        res1.push_back(old_leven(a, b));
+    }
 
-    a = "351 100 nơ trang long nơ trang long phường 13 gò vấp hồ chí minh hồ chí minh";
-    b = "bình thạnh|bt";
-    test_single(a, b);
+    assert(res1.size() == res2.size());
+    for(int i = 0 ; i < (int)res1.size(); i++)
+        assert(res1[i] == res2[i]);
+//    cout << "Percentage: " << new_duration / old_duration * 100 <<  "%" << endl;
+*/
+    cout << new_duration << " " << old_duration << endl;
 
-    a = "710 69 lũy bán bích f tân thành q tân phú hcm tân thành tân bình hồ chí minh hồ chí minh";
-    b = "phu thanh";
-    test_single(a, b);
-
-    a = "351 100 nơ trang long nơ trang long phường 13 gò vấp hồ chí minh hồ chí minh";
-    b = "bt";
-    test_single(a, b);
-
-    a = "710 69 lũy bán bích f tân thành q tân phú hcm tân thành tân bình hồ chí minh hồ chí minh\n";
-    b = "^tân thành";
-    test_single(a, b);
-
-    a = "công ty showa gloves 152 2 tổ 11 khu phố 1 b an phú thuận an bình dương bình dương";
-    b = "^an phú|^ap";
-    test_single(a, b);
-
-    a = "cong ty showa gloves 152 2 to 11 khu pho 1 b an phu thuan an binh duong binh duong";
-    b = "an phu|ap";
-    test_single(a, b);
-    cout << "Percentage: " << new_duration / old_duration * 100 <<  "%" << endl;
-
-    //
-//    ifstream fin("test_1.txt");
-//    string s;
-//    while (getline(fin, s)){
-//        cout << s << endl;
-//        s = VnLangTool::lower_root(s);
-//        int sub_len = rand() % 15 + 5;
-//
-//            string b = gen_random(sub_len);
-//            b = VnLangTool::lower_root(b);
-//            cout << s << "\n" << b << endl;
-//            test_single(s, b);
-//    }
     return 0;
 }

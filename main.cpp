4056c92da09527b3568ff874c4e4a5707d0f3fe7
Ngo Duc Huy <ngoduchuy64@gmail.com>
2020-12-10 10:20:27 +0700

    minor opt

--- main.cpp
+++ main.cpp
@@ -2,12 +2,23 @@
 #include <cstring>
 #include <fstream>
 #include <cassert>
+#include <unistd.h>
+
 #include "vn_lang_tool.hpp"
 
 using namespace std;
 
 const int N = 1005;
 int dp[N][N];
+
+
+int max(const int &a, const int &b){
+    return a > b ? a : b;
+}
+
+int min(const int &a, const int &b){
+    return a < b ? a : b;
+}
 
 int old_leven(string sa, string sb){
     string a = " " + sa;
@@ -25,7 +36,9 @@ int old_leven(string sa, string sb){
             ans = max(ans, dp[i][j]);
         }
     }
-    for(int i = 0; i < int(a.size()); i++) for(int j = 0; j < int(b.size()); j++) dp[i][j] = 0;
+    for(int i = 0; i <= int(a.size()); i++) dp[i][0] = 0;
+    for(int j = 0; j < int(b.size()); j++) dp[0][j] = 0;
+
     return ans;
 }
 
@@ -56,23 +69,34 @@ int attempt(string sa, string sb){
 
     for(int i = 1; i <= m; i++){
         for(int j = 1; j < int(b.size()); j++){
-            dp[i][j] = max(dp[i][j - 1] - 1, dp[i-1][j] - 1 - w[i]);
+            if (dp[i][j - 1] - 1 > dp[i-1][j] - 1 - w[i]) {
+                dp[i][j] = dp[i][j - 1] - 1;
+            }
+            else {
+                dp[i][j] = dp[i - 1][j] - 1;
+            }
 
-            dp[i][j] = max(dp[i][j], dp[i-1][j-1] - 1 - w[i]);
+            if (dp[i][j] < dp[i-1][j-1] - 1 - w[i]){
+                dp[i][j] = dp[i-1][j-1] - 1 - w[i];
+            }
 
             if (a[i] == b[j]) {
                 for(int l = 0, min_ = min(w[i], j - 1); l <= min_; l++) {
-                    dp[i][j] = max(dp[i][j], max(0, dp[i - 1][j - 1 - l] - w[i]) + 1);
+                    if (dp[i][j] < max(0, dp[i - 1][j - 1 - l] - w[i]) + 1){
+                        dp[i][j] = max(0, dp[i - 1][j - 1 - l] - w[i]) + 1;
+                    }
+
                 }
             }
-            dp[i][j] = max(dp[i][j], 0);
-//            cout << i << " " << j << " " << dp[i][j] << endl;
-            ans = max(ans, dp[i][j]);
+            if (dp[i][j] < 0) dp[i][j] = 0;
+            if (ans < dp[i][j]) ans = dp[i][j];
         }
     }
 
     // reset
-    for(int i = 0; i <= m; i++) for(int j = 0; j < int(b.size()); j++) dp[i][j] = 0;
+    for(int i = 0; i <= m; i++) dp[i][0] = 0;
+    for(int j = 0; j < int(b.size()); j++) dp[0][j] = 0;
+
     for(int i = 1; i < (int)b.size(); i++) {
         in_b[b[i]] = false;
         w[i] = 0;
@@ -87,21 +111,42 @@ void test_single(string a, string b){
     double duration;
 
     start = clock();
-    int new_ans =attempt(a, b);
-    cout << new_ans << endl;
-    double new_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
-    cout<< "Attempt time: "<< new_duration <<'\n';
-
-    start = clock();
     int old_ans = old_leven(a, b);
     cout << old_ans << endl;
     double old_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
     cout<< "Old time: " << old_duration <<'\n';
 
 
+    start = clock();
+    int new_ans =attempt(a, b);
+    cout << new_ans << endl;
+    double new_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
+    cout<< "Attempt time: "<< new_duration <<'\n';
+
+
+
+
     cout << "Percentage: " << new_duration / old_duration * 100 <<  "%" << endl;
     cout << "**********************\n";
     assert(new_ans == old_ans);
+}
+
+string gen_random(const int len) {
+
+    string tmp_s;
+    static const char alphanum[] =
+            "abcdeqphngo";
+
+//    srand( (unsigned) time(NULL) * getpid());
+
+    tmp_s.reserve(len);
+
+    for (int i = 0; i < len; ++i)
+        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+
+
+    return tmp_s;
+
 }
 
 int main() {
@@ -116,18 +161,55 @@ int main() {
     a = "giaoc";
     b = "ngoc";
     test_single(a, b);
+    a = "số 16 nguỵ như kon tum thanh xuân hn nhân chính hà đông hà nội hà nội";
+    b = "nhan chinh";
+    test_single(a, b);
+
+    a = "chua lang dong da ha noi ha noi";
+    b = "^láng hạ|^lh";
+    test_single(a, b);
+
+    a = "chua lang dong da ha noi ha noi";
+    b = "lh";
+    test_single(a, b);
 
 
-    ifstream fin("test_1.txt");
-    string s;
-    while (getline(fin, s)){
-        cout << s << endl;
-        int sub_len = rand() % 15 + 5;
-        for(int i = 0; i < (int)s.size() - sub_len + 1; i++){
-            string b = "doi can";
-            cout << s << "\n" << b << endl;
-            test_single(s, b);
-        }
-    }
+    a = "193 tl 19 tạp hoá văn cường gặp chị anh thạnh lộc gò vấp hồ chí minh hồ chí minh";
+    b = "^phường 16|^p1";
+    test_single(a, b);
+
+    a = "351 100 nơ trang long nơ trang long phường 13 gò vấp hồ chí minh hồ chí minh";
+    b = "bình thạnh|bt";
+    test_single(a, b);
+
+    a = "710 69 lũy bán bích f tân thành q tân phú hcm tân thành tân bình hồ chí minh hồ chí minh";
+    b = "phu thanh";
+    test_single(a, b);
+
+    a = "351 100 nơ trang long nơ trang long phường 13 gò vấp hồ chí minh hồ chí minh";
+    b = "bt";
+    test_single(a, b);
+
+    a = "710 69 lũy bán bích f tân thành q tân phú hcm tân thành tân bình hồ chí minh hồ chí minh\n";
+    b = "^tân thành";
+    test_single(a, b);
+
+    a = "công ty showa gloves 152 2 tổ 11 khu phố 1 b an phú thuận an bình dương bình dương";
+    b = "^an phú|^ap";
+    test_single(a, b);
+
+    //
+//    ifstream fin("test_1.txt");
+//    string s;
+//    while (getline(fin, s)){
+//        cout << s << endl;
+//        s = VnLangTool::lower_root(s);
+//        int sub_len = rand() % 15 + 5;
+//
+//            string b = gen_random(sub_len);
+//            b = VnLangTool::lower_root(b);
+//            cout << s << "\n" << b << endl;
+//            test_single(s, b);
+//    }
     return 0;
 }

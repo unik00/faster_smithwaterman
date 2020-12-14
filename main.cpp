8e24fc58974df5a825d59b9caa86f3ff4d65b1b1
Ngo Duc Huy <ngoduchuy64@gmail.com>
2020-12-14 10:32:37 +0700

    wtf

--- main.cpp
+++ main.cpp
@@ -21,10 +21,53 @@ int min(const int &a, const int &b){
     return a < b ? a : b;
 }
 
+int find_substring(string str, string pattern) {
+
+    // Step 0. Should not be empty string
+    if( str.size() == 0 || pattern.size() == 0)
+        return -1;
+
+    // Step 1. Compute failure function
+    int failure[pattern.size()];
+    std::fill( failure, failure+pattern.size(), -1);
+
+    for(int r=1, l=-1; r<pattern.size(); r++) {
+
+        while( l != -1 && pattern[l+1] != pattern[r])
+            l = failure[l];
+
+        // assert( l == -1 || pattern[l+1] == pattern[r]);
+        if( pattern[l+1] == pattern[r])
+            failure[r] = ++l;
+    }
+
+    // Step 2. Search pattern
+    int tail = -1;
+    for(int i=0; i<str.size(); i++) {
+
+        while( tail != -1 && str[i] != pattern[tail+1])
+            tail = failure[tail];
+
+        if( str[i] == pattern[tail+1])
+            tail++;
+
+        if( tail == pattern.size()-1)
+            return i - tail;
+    }
+
+    return -1;
+}
+
 int old_leven(string sa, string sb){
     clock_t start;
-    double duration;
     start = clock();
+
+//    if (find_substring(sa, sb) == sb.length()) {
+//        cout << sa<<endl <<sb<<endl;
+//        double s_old_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
+//        old_duration += s_old_duration;
+//        return sb.length();
+//    }
 
     string a = " " + sa;
     string b = " " + sb;
@@ -48,9 +91,9 @@ int old_leven(string sa, string sb){
 //                cout << i << " " << j << " " << i-1 << " "<< j - 1 << endl;
 
             }
-            dp[i][j] = max(dp[i][j], 0);
-//            cout << i << " " << j << " " << dp[i][j] << endl;
-            ans = max(ans, dp[i][j]);
+            if (dp[i][j] < 0) dp[i][j] = 0;
+            //            cout << i << " " << j << " " << dp[i][j] << endl;
+            if (ans < dp[i][j]) ans=dp[i][j];
         }
     }
     for(int i = 0; i <= int(a.size()); i++) dp[i][0] = 0;
@@ -79,9 +122,15 @@ int query(const int *f, const int &n, int x){
 
 int attempt(string sa, string sb){
     clock_t start;
-    double duration;
     start = clock();
-
+    /*
+    if (find_substring(sa, sb) == sb.length()) {
+//        cout << sa<<endl <<sb<<endl;
+        double s_old_duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
+        old_duration += s_old_duration;
+        return sb.length();
+    }
+      */
     string b = " " + sb;
     string _a = sa;
 
@@ -104,10 +153,10 @@ int attempt(string sa, string sb){
 //    cout << "a: "; for (int i = 1; i <=m; i++) cout <<a[i]; cout<<endl;
 //    for (int i = 1; i <=m; i++) cout <<w[i] << " "; cout<<endl;
 //    cout << "b: " << b << endl;
-    int fnw[(int)b.size()];
+//    int fnw[(int)b.size()];
 
     for(int i = 1; i <= m; i++){
-        memset(fnw, 0, sizeof(fnw));
+//        memset(fnw, 0, sizeof(fnw));
         for(int j = 1; j < int(b.size()); j++){
             if (dp[i][j - 1] - 1 > dp[i-1][j] - w[i] - 1) {
                 dp[i][j] = dp[i][j - 1] - 1;
@@ -116,23 +165,22 @@ int attempt(string sa, string sb){
                 dp[i][j] = dp[i - 1][j] - w[i] - 1;
             }
 
-            int cur = query(fnw, (int)b.size() - 1, max(1, j - 1 - w[i]));
-            int tw = (a[i] == b[j]) ? 1 : -1;
-            if (dp[i][j] < max(0, cur - w[i]) + tw) {
-                dp[i][j] = max(0, cur - w[i]) + tw;
-            }
-
-            // we need to query max(dp[i-1][j-1-w[i] to j-1]), can we do this faster?
-//            for(int l = 0, min_ = min(w[i], j - 1); l <= min_; l++) {
-//                int tw = (a[i] == b[j]) ? 1 : -1;
-//                if (dp[i][j] < max(0, dp[i - 1][j - 1 - l] - w[i]) + tw){
-//                    dp[i][j] = max(0, dp[i - 1][j - 1 - l] - w[i]) + tw;
-//                }
+//            int cur = query(fnw, (int)b.size() - 1, max(1, j - 1 - w[i]));
+//            int tw = (a[i] == b[j]) ? 1 : -1;
+//            if (dp[i][j] < max(0, cur - w[i]) + tw) {
+//                dp[i][j] = max(0, cur - w[i]) + tw;
 //            }
+//             we need to query max(dp[i-1][j-1-w[i] to j-1]), can we do this faster?
+            for(int l = 0, min_ = min(w[i], j - 1); l <= min_; l++) {
+                int tw = (a[i] == b[j]) ? 1 : -1;
+                if (dp[i][j] < max(0, dp[i - 1][j - 1 - l] - w[i]) + tw){
+                    dp[i][j] = max(0, dp[i - 1][j - 1 - l] - w[i]) + tw;
+                }
+            }
 
             if (dp[i][j] < 0) dp[i][j] = 0;
             if (ans < dp[i][j]) ans = dp[i][j];
-            upd(fnw, j, dp[i-1][j]);
+//            upd(fnw, j, dp[i-1][j]);
         }
     }
 
@@ -179,33 +227,39 @@ int main() {
 
     while (getline(fin, a)){
         getline(fin, b);
-        a = VnLangTool::lower_root(a);
-        b = VnLangTool::lower_root(b);
+        b.erase(std::remove(b.begin(), b.end(), '^'), b.end());
+
+//        a = VnLangTool::lower_root(a);
+//        b = VnLangTool::lower_root(b);
 //        int x = old_leven(a, b);
 //        int y = attempt(a, b);
 //        cout << a << endl << b << endl;
 //        cout << x << " " << y << endl;
 //        assert(x == y);
-        res1.push_back(old_leven(a, b));
+        auto bs = VnLangTool::split_str(b, "|");
+        for(auto s: bs) res2.push_back(attempt(a, s));
 
     }
     fin.close();
-
     fin.open("test_1.txt");
     while (getline(fin, a)){
         getline(fin, b);
-        a = VnLangTool::lower_root(a);
-        b = VnLangTool::lower_root(b);
-        res2.push_back(attempt(a, b));
+        b.erase(std::remove(b.begin(), b.end(), '^'), b.end());
 
+//        a = VnLangTool::lower_root(a);
+//        b = VnLangTool::lower_root(b);
+        auto bs = VnLangTool::split_str(b, "|");
+        for(auto s: bs) {
+//            cout << a << endl << s << endl;
+            res1.push_back(old_leven(a, s));
+        }
     }
 
     assert(res1.size() == res2.size());
     for(int i = 0 ; i < (int)res1.size(); i++)
         assert(res1[i] == res2[i]);
     cout << "Percentage: " << new_duration / old_duration * 100 <<  "%" << endl;
-
-
+    cout << new_duration / res2.size() << " " << old_duration / res1.size() << endl;
 //    for(int i = 0; i < int(1e6); i++){
 //        a = gen_random(7);
 //        b = gen_random(7);
